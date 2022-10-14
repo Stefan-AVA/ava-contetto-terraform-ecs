1d0185405841a378e700f80bf389e700c3c32679
Christopher Trimboli <cjft@trimboli.io>
2022-10-14 14:15:13 -0600

    change .env path

--- build_image.sh
+++ build_image.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "${ENV_FILE}" > ../.env
-cat ../.env
+echo "${ENV_FILE}" > .env
 
 ecr=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 image=${ecr}/${SERVICE_NAME}_${CI_COMMIT_REF_NAME}:latest
 
-docker build -t ${image} ./
+docker build -t ${image} .
