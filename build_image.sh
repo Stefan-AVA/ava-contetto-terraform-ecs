1dc3a9c819c73a81f4ccee58b356e87ccdd8c029
Christopher Trimboli <cjft@trimboli.io>
2022-10-14 12:32:49 -0600

    cd backwards before copy env

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export env_file=env_${CI_COMMIT_REF_NAME}
-cp ${!env_file} ../.env
+cd ..
+cp ${!env_file} .env
 
 ecr=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 image=${ecr}/${SERVICE_NAME}_${CI_COMMIT_REF_NAME}:latest
