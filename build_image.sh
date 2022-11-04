d2fe4334e80f131630c774fa690568c5835946d1
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-03 22:16:47 -0600

    write NEXT_ENV_FILE in build_image step

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "${ENV_FILE}" > .env
+echo "${NEXT_ENV_FILE}" > .env.local
 
 ecr=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 image=${ecr}/${SERVICE_NAME}_${CI_COMMIT_REF_NAME}:latest
